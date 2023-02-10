b2a294ce4fadf713af320ecaeee70432c64db8ea
IzaFreire <kariny.frei.2000@gmail.com>
2023-02-09 22:37:05 -0300

    teste

--- src/schemas/Customers.model.js
+++ src/schemas/Customers.model.js
@@ -3,6 +3,6 @@ import Joi from "joi";
 export const CustomersSchema = Joi.object({
     name: Joi.string().min(3).required(),
     phone: Joi.string().min(10).max(11).required(),
-    cpf: Joi.string().min(11).required(),
+    cpf: Joi.string().regex(/^\d{11}$/).required(),
     birthday: Joi.date().required()
 });
