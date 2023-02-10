99b435076903b05b2d42e6b518dbb62afec35ff1
IzaFreire <kariny.frei.2000@gmail.com>
2023-02-09 22:46:07 -0300

    ajuste

--- src/routers/customer.routes.js
+++ src/routers/customer.routes.js
@@ -8,6 +8,6 @@ const router = Router();
 router.post("/customers", validShemaCustomers, createCustomer);
 router.get("/customers", retrieveCustomers);
 router.get("/customers/:id", getCustomerById);
-router.put("/customers/:id", putUpdateCustomer, validShemaCustomers);
+router.put("/customers/:id", validShemaCustomers, putUpdateCustomer);
 
 export default router;
